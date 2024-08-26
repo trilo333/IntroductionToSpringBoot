f60f6c48f802a22bc1153c57184618635c7c9852
Trilochan Yadav <tytrilochanyadav@gmail.com>
2024-08-26 23:09:27 +1000

    1.2 Setting Up

---
+++ src/main/java/com/codingshuttle/trilochan/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.codingshuttle.trilochan.week1Introduction.introductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.System;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+}
